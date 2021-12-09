0737e6125430332281562196182533d0c0d8f3e2
moataz <moatazmwise@mail.tau.ac.il>
2021-12-09 17:01:44 +0200

    first commit

---
+++ Assets/scripts/Machine.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Port
+{
+    disabled,
+    itemInput,
+    itemOutput,
+    energyInput,
+    energyOutput
+}
+
+public class Machine : Block
+{
+    public int[] inventorySizes;
+    public Port[] portTypes;
+    public int[] portConnections;
+    public int[] faces = new int[6];
+    
+    private Inventory[] inventories;
+    [HideInInspector] public int energyInput;
+    [HideInInspector] public int energyOutput;
+
+    public virtual void Awake()
+    {
+        inventories = new Inventory[inventorySizes.Length];
+        for (int i = 0; i< inventories.Length; i++)
+        {
+            inventories[i] = new Inventory(inventorySizes[i]);
+            Debug.Log(inventories[i].size);
+        }
+    }
+
+}
