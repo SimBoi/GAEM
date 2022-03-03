ab8869da83bd211602ad6bbe2c1f484da20f4f87
moataz <moatazmwise@mail.tau.ac.il>
2022-03-03 18:15:51 +0200

    Worms

--- Assets/scripts/ChunkGenerator.cs
+++ Assets/scripts/ChunkGenerator.cs
@@ -8,11 +8,12 @@ public class ChunkGenerator : MonoBehaviour
 {
     public Land land;
     public int landSize;
-    public bool Gen3d;
+    public bool GenCave;
 
     private void Start()
     {
-        Generate();
+        if (!GenCave) Generate();
+        else GenerateCave();
     }
     /*
     public void GenerateChunks()
@@ -117,36 +118,91 @@ public class ChunkGenerator : MonoBehaviour
         }
     }
 
-/*    public void Generate3d()
+    public int worms = 1;
+    public int wormLength = 50;
+    public float wormNoiseFrequency = 1;
+    public int wormScale = 1;
+
+    //public void GenerateCave(Vector3Int chunkStartPosition, int chunkWidth, int maxHeight)
+    public void GenerateCave()
     {
-        if (randomizeNoiseOffset)
+        int blockSize = landSize * land.chunkSizeX;
+        
+        for (int wormCount = worms; wormCount > 0; wormCount--)
         {
-            perlinOffset = new Vector3(Random.Range(0, 9999), Random.Range(0, 9999), Random.Range(0, 9999));
-        }
-        for (int x = 0; x < blockSize; x++)
-        {
-            for (int y = 0; y < landSize * land.chunkSizeY; y++)
+            int x = (int)Random.Range(0, blockSize);
+            int y = (int)Random.Range(0, blockSize);
+            int z = (int)Random.Range(0, blockSize);
+
+            float wx = Mathf.Clamp(Mathf.PerlinNoise(x * wormNoiseFrequency, x * wormNoiseFrequency), 0, 1);
+            float wy = Mathf.Clamp(Mathf.PerlinNoise(y * wormNoiseFrequency, y * wormNoiseFrequency), 0, 1);
+            float wz = Mathf.Clamp(Mathf.PerlinNoise(z * wormNoiseFrequency, z * wormNoiseFrequency), 0, 1);
+
+            for (int wormLengthCount = wormLength; wormLengthCount > 0; wormLengthCount--)
             {
-                for (int z = 0; z < landSize * land.chunkSizeZ; z++)
+                Debug.Log(new Vector3Int(x, y, z));
+
+                wx = Mathf.Clamp(Mathf.PerlinNoise(x * wx * wormNoiseFrequency, x * (1 / wx) * wormNoiseFrequency), 0, 1);
+                wy = Mathf.Clamp(Mathf.PerlinNoise(y * wy * wormNoiseFrequency, y * (1 / wy) * wormNoiseFrequency), 0, 1);
+                wz = Mathf.Clamp(Mathf.PerlinNoise(z * wz * wormNoiseFrequency, z * (1 / wz) * wormNoiseFrequency), 0, 1);
+
+                x = Mathf.Clamp(WormMovement(wx, x), 0, blockSize);
+                y = Mathf.Clamp(WormMovement(wy, y), 0, 256);
+                z = Mathf.Clamp(WormMovement(wz, z), 0, blockSize);
+
+                for (int xcord = x - (int)wormScale / 2; xcord <= x + wormScale / 2; xcord++)
                 {
-                    float generated = GenerateNoise(((float)x + perlinOffset.x) * noiseScale, ((float)y + perlinOffset.y)* noiseScale, ((float)z + perlinOffset.z) * noiseScale);
-                    if (generated > 0.5f) land.AddBlock(new Vector3Int(x,y,z), (short)2);
+                    for (int ycord = y - (int)wormScale / 2; ycord <= y + wormScale / 2; ycord++)
+                    {
+                        for (int zcord = z - (int)wormScale / 2; zcord <= z + wormScale / 2; zcord++)
+                        {
+                            Vector3Int coordVector = new Vector3Int(Mathf.Clamp(xcord,0, blockSize), Mathf.Clamp(ycord, 0, blockSize), Mathf.Clamp(zcord, 0, blockSize));
+                            land.AddBlock(coordVector, (short)2);
+                        }
+                    }
                 }
             }
         }
     }
 
-    public float GenerateNoise(float x, float y, float z)
+    public int WormMovement(float noise , int currPos)
     {
-        float XY = Mathf.PerlinNoise(x, y);
-        float YZ = Mathf.PerlinNoise(y, z);
-        float ZX = Mathf.PerlinNoise(z, x);
-
-        float YX = Mathf.PerlinNoise(y, z);
-        float ZY = Mathf.PerlinNoise(z, y);
-        float XZ = Mathf.PerlinNoise(x, z);
-
-        float val = (XY + YZ + ZX + YX + ZY + XZ)/6f;
-        return val;
+        if (noise > 0.5) return currPos + 1;
+        if (noise <= 0.5) return currPos - 1;
+        return currPos - 1;
     }
-*/}
+
+        /*    public void Generate3d()
+            {
+                if (randomizeNoiseOffset)
+                {
+                    perlinOffset = new Vector3(Random.Range(0, 9999), Random.Range(0, 9999), Random.Range(0, 9999));
+                }
+                for (int x = 0; x < blockSize; x++)
+                {
+                    for (int y = 0; y < landSize * land.chunkSizeY; y++)
+                    {
+                        for (int z = 0; z < landSize * land.chunkSizeZ; z++)
+                        {
+                            float generated = GenerateNoise(((float)x + perlinOffset.x) * noiseScale, ((float)y + perlinOffset.y)* noiseScale, ((float)z + perlinOffset.z) * noiseScale);
+                            if (generated > 0.5f) land.AddBlock(new Vector3Int(x,y,z), (short)2);
+                        }
+                    }
+                }
+            }
+
+            public float GenerateNoise(float x, float y, float z)
+            {
+                float XY = Mathf.PerlinNoise(x, y);
+                float YZ = Mathf.PerlinNoise(y, z);
+                float ZX = Mathf.PerlinNoise(z, x);
+
+                float YX = Mathf.PerlinNoise(y, z);
+                float ZY = Mathf.PerlinNoise(z, y);
+                float XZ = Mathf.PerlinNoise(x, z);
+
+                float val = (XY + YZ + ZX + YX + ZY + XZ)/6f;
+                return val;
+            }
+        */
+    }
